81587b5644fc441b6bdb65aade5c3b3b525bd66a
Neil Khera <32021246+Nekhera@users.noreply.github.com>
2020-09-21 19:16:56 -0400

    Fixed some bugs

--- moonlight.py
+++ moonlight.py
@@ -6,8 +6,12 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 Image.MAX_IMAGE_PIXELS = None
-heightmap = np.array(Image.open('small_dem.tif'), dtype='uint16')
+heightmap = np.array(Image.open('test.png'), dtype='int16')
+lightmap = np.zeros(heightmap.shape)
 scale = 1
+
+direction = (1, 1)
+inclination = 10
 
 line_stack = []
 
@@ -19,51 +23,54 @@ def inBounds(row, col):
 
   return True
 
-def lineWalk(direction, row, col):
+def lineWalk(row, col):
   r = row
   c = col
 
-  dx = direction[0]
-  dy = direction[1]
+  dr = direction[0]
+  dc = direction[1]
 
   while (inBounds(r, c)):
     yield (int(r), int(c))
-    r = r + dx
-    c = c + dy
+    r = r + dr
+    c = c + dc
 
-def clearStack(val):
+def clearStack():
   while len(line_stack) != 0:
     coords = line_stack.pop()
-    heightmap[coords[0], coords[1]] = val
+    lightmap[coords[0], coords[1]] = -50
 
-def sunlit(direction, inclination):
+def sunlit():
   tan_incl = math.tan(inclination * math.pi / 180.0)
 
   for row in range(heightmap.shape[0]):
     for col in range(heightmap.shape[1]):
       start_point = (row, col)
 
-      if (heightmap[row, col] != 0 and heightmap[row, col] != 1):
-        for xy in lineWalk(direction, row, col):
-          horizontal_displacement = math.sqrt((xy[0] - start_point[0])**2 + (xy[1] - start_point[1])**2) * scale
+      if col == 0:
+        print(heightmap[row])
+
+      if (lightmap[row, col] != -50 and lightmap[row, col] != -10):
+        for xy in lineWalk(row, col):
+          horizontal_displacement = math.sqrt((xy[0] - start_point[0]) ** 2 + (xy[1] - start_point[1]) ** 2) * scale
           vertical_displacement = heightmap[xy[0], xy[1]] - heightmap[start_point[0], start_point[1]]
           
           if vertical_displacement > (tan_incl * horizontal_displacement):
-            clearStack(0)
+            clearStack()
             start_point = xy
 
           line_stack.append(xy)
 
-        clearStack(1)
+        lightmap[start_point[0], start_point[1]] = -10
+        line_stack.clear()
 
 if __name__ == "__main__":
-  direction = (1, 0)
-  inclination = 20
 
-  sunlit(direction, inclination)
-  plt.imshow(heightmap)
+  sunlit()
+  print(lightmap)
+  plt.imshow(lightmap)
   plt.show()
 
-  from mayavi import mlab
-  mlab.surf(heightmap / 10)
-  mlab.show()
+  #from mayavi import mlab
+  #mlab.surf(heightmap / 10)
+  #mlab.show()
