1350bda97cc1e6bcc4c48ca832d67d0d402157aa
Neil Khera <32021246+Nekhera@users.noreply.github.com>
2020-09-22 15:39:45 -0400

    Cleaned up code

--- moonlight.py
+++ moonlight.py
@@ -4,21 +4,10 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
-
 Image.MAX_IMAGE_PIXELS = None
-dem = Image.open('moon.png')
-heightmap = np.array(dem, dtype='int16')
-heightmap = np.array(heightmap / 2, dtype='float32')
-lightmap = np.zeros(heightmap.shape)
-scale = 5
-
-direction = (-0.985, -0.174)
-inclination = 15
-
-line_stack = []
 
 def inBounds(row, col):
-  if row >= heightmap.shape[0] or col >= heightmap.shape[1]:
+  if row >= dem_data[0].shape[0] or col >= dem_data[0].shape[1]:
     return False
   if row < 0 or col < 0:
     return False
@@ -29,33 +18,32 @@ def lineWalk(row, col):
   r = row
   c = col
 
-  dr = direction[0]
-  dc = direction[1]
+  dr = sun_direction[0]
+  dc = sun_direction[1]
 
   while (inBounds(r, c)):
     yield (int(r), int(c))
     r = r + dr
     c = c + dc
 
-def clearStack():
-  while len(line_stack) != 0:
-    coords = line_stack.pop()
-    lightmap[coords[0], coords[1]] = -50
+def moonlight(dem_data, sun_position):
+  tan_incl = math.tan(sun_position[1] * math.pi / 180.0)
+  lightmap = np.zeros(dem_data[0].shape)
 
-def sunlit():
-  tan_incl = math.tan(inclination * math.pi / 180.0)
-
-  for row in range(heightmap.shape[0]):
-    for col in range(heightmap.shape[1]):
+  for row in range(lightmap.shape[0]):
+    for col in range(lightmap.shape[1]):
       start_point = (row, col)
 
       if (lightmap[row, col] != -50 and lightmap[row, col] != -10):
         for xy in lineWalk(row, col):
-          horizontal_displacement = math.sqrt((xy[0] - start_point[0]) ** 2 + (xy[1] - start_point[1]) ** 2) * scale
-          vertical_displacement = heightmap[xy[0], xy[1]] - heightmap[start_point[0], start_point[1]]
+          horizontal_displacement = math.sqrt((xy[0] - start_point[0]) ** 2 + (xy[1] - start_point[1]) ** 2) * dem_data[1]
+          vertical_displacement = dem_data[0][xy[0], xy[1]] - dem_data[0][start_point[0], start_point[1]]
 
           if vertical_displacement > (tan_incl * horizontal_displacement):
-            clearStack()
+            while len(line_stack) != 0:
+              coords = line_stack.pop()
+              lightmap[coords[0], coords[1]] = -50
+
             start_point = xy
 
           line_stack.append(xy)
@@ -63,9 +51,19 @@ def sunlit():
         lightmap[start_point[0], start_point[1]] = -10
         line_stack.clear()
 
+  return lightmap
+
 if __name__ == "__main__":
-  plt.imshow(heightmap)
-  plt.show()
-  sunlit()
+  dem_heightmap = np.array(Image.open('moon.png'), dtype='float32')
+  dem_scale = 5
+  dem_data = (dem_heightmap, dem_scale)
+
+  sun_direction = (-1, 0)
+  sun_inclination = 15
+  sun_position = (sun_direction, sun_inclination)
+
+  line_stack = []
+
+  lightmap = moonlight(dem_data, sun_position)
   plt.imshow(lightmap)
   plt.show()
